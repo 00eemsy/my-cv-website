c0076a076910509365da1a1949a5563977154013
Emily Yu <emilyyu415@Emilys-MacBook-Air-2.local>
2024-06-20 20:28:10 -0700

    aka shan-yi subtext added! <3

--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -5,7 +5,9 @@ import "./title.css";
 const TitlePage = (() => {
     return (
         <div className='title'>
-            <div className='two-column'>
+
+            <div className='two-column top-two-column'>
+
                 <div>
                 </div>
 
@@ -15,15 +17,37 @@ const TitlePage = (() => {
                         hello, world!
                     </h3>
                     <h3 className='pixel'>
-                        welcome to my world! my name is: 
+                        welcome to my world. my name is: 
                     </h3>
-                    
+
                 </div>
-                
+
             </div>
+
             <h1>
-                <span className='cursive'>E</span><span className='pixel'>MILY YU</span>
+                <span className='cursive header1-cursive'>E</span><span className='pixel header1-pixel'>MILY YU</span>
             </h1>
+
+            <div className='two-column bottom-two-column'>
+
+                <div className='two-row'>
+
+                    {/* <div className='spacer'>
+                    </div> */}
+
+                    <h3 className='pixel'>
+                        a.k.a.
+                    </h3>
+                    <h2>
+                        <span className='cursive header2-cursive'>S</span>    <span className='pixel header2-pixel'>HAN-YI</span>
+                    </h2>
+
+                </div>
+
+                <div>
+                </div>
+
+            </div>
         </div>
     )
 });
