a361a78be1563e7356c7eb8448d3d14b3bf4cb99
00eemsy <eyu19415@gmail.com>
2024-10-27 14:14:19 -0700

    more routing?? maybe?

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
+    {/* <App/> */}
+
+    <nav>
+      <Link to='/'></Link>
+    </nav>
+
+    <Routes>
+      <Route exact path='/' element={<App />}/>
+    </Routes>
   </BrowserRouter>
 );
 
