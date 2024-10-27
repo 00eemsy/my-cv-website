21696d66628d88f6748955e16383f3842e6e2931
00eemsy <eyu19415@gmail.com>
2024-10-27 14:31:53 -0700

    updating homepage?

--- src/App.js
+++ src/App.js
@@ -3,31 +3,15 @@ import TitlePage from './components/title/title'
 import ElevatorPitch from './components/elevatorPitch/elevatorPitch';
 import Experience from './components/experience/experience';
 import Projects from './components/projects/projects';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <nav>
-        <Link to='/'></Link>
-      </nav>
-
-      <Routes>
-        <Route exact path='/' 
-        element={<div>
-          <TitlePage />
-          <ElevatorPitch />
-          <Experience />
-          <Projects />
-        </div>}></Route>
-      </Routes>
-    </BrowserRouter>
-    // <div>
-    //     <TitlePage />
-    //     <ElevatorPitch />
-    //     <Experience />
-    //     <Projects />
-    // </div>
+    <div>
+        <TitlePage />
+        <ElevatorPitch />
+        <Experience />
+        <Projects />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <BrowserRouter basename={process.env.PUBLIC_URL}>
-  //   {/* <App/> */}
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    {/* <App/> */}
 
-  //   <nav>
-  //     <Link to='/'></Link>
-  //   </nav>
+    <nav>
+      <Link to='/'></Link>
+    </nav>
 
-  //   <Routes>
-  //     <Route exact path='/' element={<App />}/>
-  //   </Routes>
-  // </BrowserRouter>
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <Routes>
+      <Route exact path='/' element={<App />}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
