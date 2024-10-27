c8bca65bc842b35212d7dc0badf1172037529094
00eemsy <eyu19415@gmail.com>
2024-10-27 14:22:56 -0700

    trying browserrouter in app.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    {/* <App/> */}
+  // <BrowserRouter basename={process.env.PUBLIC_URL}>
+  //   {/* <App/> */}
 
-    <nav>
-      <Link to='/'></Link>
-    </nav>
+  //   <nav>
+  //     <Link to='/'></Link>
+  //   </nav>
 
-    <Routes>
-      <Route exact path='/' element={<App />}/>
-    </Routes>
-  </BrowserRouter>
+  //   <Routes>
+  //     <Route exact path='/' element={<App />}/>
+  //   </Routes>
+  // </BrowserRouter>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
